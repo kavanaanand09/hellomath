c2c932f7ac4e3167579ad3ad8c775ccd18bef0fd
kaanand <kaanand@adobe.com>
2019-08-26 15:42:16 -0700

    intial commit

---
+++ hellomath/hellomath/test_basic.cpp
@@ -0,0 +1,22 @@
+//
+//  test_basic.cpp
+//  hellomath
+//
+//  Created by Kavana Anand on 8/26/19.
+//  Copyright © 2019 Kavana Anand. All rights reserved.
+//
+
+#include "test_basic.hpp"
+
+
+namespace hellomath {
+	
+	int add(int a, int b) {
+		return a + b;
+	}
+	
+	int sub(int a, int b) {
+		return a - b;
+	}
+	
+}
---
+++ hellomath/hellomath/test_basic.hpp
@@ -0,0 +1,24 @@
+//
+//  test_basic.hpp
+//  hellomath
+//
+//  Created by Kavana Anand on 8/26/19.
+//  Copyright © 2019 Kavana Anand. All rights reserved.
+//
+
+#ifndef test_basic_hpp
+#define test_basic_hpp
+
+#include <stdio.h>
+
+
+namespace hellomath {
+	
+	int add(int a, int b);
+	
+	int sub(int a, int b);
+	
+}
+
+
+#endif /* test_basic_hpp */
